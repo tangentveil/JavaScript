f3d85bedcab70e4e0fccf3448fd149755c0312e3
tangentveil <anirudha0789@gmail.com>
2024-10-05 16:04:38 +0530

    first commit

---
+++ Functions.js
@@ -0,0 +1,17 @@
+console.log("Functions");
+
+// function greet(name, time){
+//     console.log(name + " kdmflksdmflksd " + time)
+// }
+
+// let name = "Anirudha";
+// let time = "15:11";
+// greet(name, time);
+
+function sum(a,b){
+    return a+b;
+}
+
+let a = 1;
+let b = 2;
+console.log(sum(a,b));
